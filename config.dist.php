16d3f906a20f6ae67b5c6a338a4d2a63b3b546b5
Tim Schumacher <tim.schumacher@tourevo.com>
2013-10-16 09:38:49 +0200

    Added a example config

---
+++ config.dist.php
@@ -0,0 +1,16 @@
+<?php
+$config = array(
+  'prosody' => array(
+    'http_base' => 'http://127.0.0.1:5280',
+    'http_host' => 'boese-ban.de:5280',
+    'url_prefix' => '/admin_rest/',
+    'user' => 'foo@localhost',
+    'password' => '',
+  ),
+  'from' => 'foo@example.com',
+  'hosts' => array(
+    'localhost',
+    'localhost2',
+  ),
+  'url' => 'https://localhost',
+);
